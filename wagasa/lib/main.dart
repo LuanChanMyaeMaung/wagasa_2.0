bfcf66eae146c9897511287103ac61a67770c0bc
Luan Chan Myae Maung <liuyoung@Luans-MacBook-Pro.local>
2023-08-01 05:08:34 +0630

    first commit

---
+++ wagasa/lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:wagasa/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 5),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const HomeScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: const Text(
+        "Wagasa",
+        style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Trebuc',
+            color: Color(0xff7548b8)),
+      )
+              .animate(onPlay: (controller) => controller.repeat())
+              .shimmer(duration: 1500.ms)),
+    );
+  }
+}
